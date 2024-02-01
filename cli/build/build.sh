3c12e15366bfd50a336006b1bbe54188d9ee5892
whoisnian <zhuchangbao1998@gmail.com>
2024-02-02 01:16:30 +0800

    OPT: build.sh usage

--- cli/build/build.sh
+++ cli/build/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 #########################################################################
 # File Name: build.sh
 # Author: nian
@@ -6,10 +6,6 @@
 # Mail: zhuchangbao1998@gmail.com
 # Created Time: 2024年02月01日 星期四 00时06分56秒
 #########################################################################
-# usage:
-#   ./build.sh .              # build for current platform
-#   ./build.sh all            # build for all platforms (linux-amd64, linux-arm64, windows-amd64, windows-arm64)
-#   ./build.sh windows amd64  # build for specified platform
 
 SCRIPT_DIR=$(dirname "$0")
 SOURCE_DIR="$SCRIPT_DIR/.."
@@ -17,7 +13,7 @@ SOURCE_DIR="$SCRIPT_DIR/.."
 APP_NAME="cli"
 BUILDTIME=$(date --iso-8601=seconds)
 if [[ -z "$GITHUB_REF_NAME" ]]; then
-  VERSION=$(git describe --tags || echo unknown)
+  VERSION=$(git describe --tags 2> /dev/null || echo unknown)
 else
   VERSION=$GITHUB_REF_NAME
 fi
@@ -40,6 +36,17 @@ elif [[ "$1" == 'all' ]]; then
 elif [[ "$#" == 2 ]]; then
   goBuild "$1" "$2" "${APP_NAME}-$1-$2-${VERSION}"
 else
-  echo "invalid build arguments"
+  cat << EOF
+Usage:
+  $(basename $0) .            # build for current platform $(go env GOOS)-$(go env GOARCH)
+  $(basename $0) all          # build for all supported platforms
+  $(basename $0) darwin amd64 # build for specified platform
+
+Supported platforms:
+  linux-amd64
+  linux-arm64
+  windows-amd64
+  windows-arm64
+EOF
   exit 1
 fi
