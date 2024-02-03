2f21d8be9526ff43e17618fde05ada96314d353e
whoisnian <zhuchangbao1998@gmail.com>
2024-02-04 00:58:35 +0800

    ADD: config and logger for server

---
+++ server/global/config.go
@@ -0,0 +1,25 @@
+package global
+
+import (
+	"github.com/whoisnian/glb/config"
+)
+
+var CFG Config
+
+type Config struct {
+	Debug  bool   `flag:"d,false,Enable debug output"`
+	LogFmt string `flag:"log,nano,Log output format, one of nano, text and json"`
+
+	FirstRun bool `flag:"first,false,Initialize database and quit in the first run"`
+	Version  bool `flag:"v,false,Show version and quit"`
+
+	ListenAddr  string `flag:"l,127.0.0.1:9000,Server listen addr"`
+	DatabaseURI string `flag:"db,postgresql://postgres@127.0.0.1/dbname,PostgreSQL database connection URI"`
+}
+
+func SetupConfig() {
+	err := config.FromCommandLine(&CFG)
+	if err != nil {
+		panic(err)
+	}
+}
--- server/main.go
+++ server/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/whoisnian/go-templates/server/global"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	global.SetupConfig()
+	global.SetupLogger()
+	global.LOG.Debugf("use config: %+v", global.CFG)
+
+	if global.CFG.Version {
+		fmt.Printf("%s %s(%s)\n", global.AppName, global.Version, global.BuildTime)
+		return
+	}
+	if global.CFG.FirstRun {
+		// TODO: initialize database
+		return
+	}
 }
